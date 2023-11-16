b98c3cab51aa2dc78ab0f4b0179dc8ddeb5d89b3
Selina Karlin <karlinselina@gmail.com>
2023-11-16 16:00:26 +0100

    feat: toggleMenu regulating overflow (-) scrollToSection

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from "@angular/common";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,17 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  isActivated: boolean;
+  isActivated = false;
   links = document.getElementById('menuLinks');
 
-  constructor(){
-    this.isActivated = false;
-    console.log(this.isActivated);
-  }
+  constructor(public router: Router, private viewportScroller: ViewportScroller) { }
 
   toggleMenu() {
+    const HTMLbody = document.querySelector('body');
     this.isActivated = !this.isActivated;
     console.log(this.isActivated);
+    if (HTMLbody && this.isActivated) {
+      HTMLbody.style.overflowY = 'hidden';
+      // HTMLbody.classList.add('no-scroll');
+      console.log('Container existiert!');
+    } else if (HTMLbody && !this.isActivated) {
+      HTMLbody.style.overflowY = 'auto'
+      // HTMLbody.classList.remove('no-scroll')
+    }
+  }
+
+  scrollToSection(id: string) {
+    this.isActivated = false;
+    this.viewportScroller.scrollToAnchor(id);
   }
 
 }
