dc7b8a443b05f27f2397593d91de8dd1a98b82de
Selina Karlin <karlinselina@gmail.com>
2023-11-16 15:59:06 +0100

    building object for rendering in DOM

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  skillsList = [
+    {
+      url: '../../assets/img/icons/0_angular.png',
+      name: 'Angular'
+    },
+    {
+      url: '../../assets/img/icons/1_typescript.png',
+      name: 'Typescript'
+    },
+    {
+      url: '../../assets/img/icons/2_javascript.png',
+      name: 'JavaScript'
+    },
+    {
+      url: '../../assets/img/icons/3_html.png',
+      name: 'HTML'
+    },
+    {
+      url: '../../assets/img/icons/4_css.png',
+      name: 'CSS'
+    },
+    {
+      url: '../../assets/img/icons/5_firebase.png',
+      name: 'Firebase'
+    },
+    {
+      url: '../../assets/img/icons/6_git.png',
+      name: 'Git'
+    },
+    {
+      url: '../../assets/img/icons/7_api.png',
+      name: 'Rest-Api'
+    },
+    {
+      url: '../../assets/img/icons/8_scrum.png',
+      name: 'Scrum'
+    },
+    {
+      url: '../../assets/img/icons/9_materialDesin.png',
+      name: 'Material Design'
+    }
+  ]
 }
