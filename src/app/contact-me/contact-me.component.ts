094e32334a90b35ea0e8aebd4b3a390d272ea758
Selina Karlin <karlinselina@gmail.com>
2023-11-22 16:34:31 +0100

    implementing form stylen basic functions

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-contact-me',
@@ -7,13 +7,14 @@ import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from "@ang
 })
 export class ContactMeComponent {
   contactForm!: FormGroup
-
+  messageSucceed: boolean = false;
   // constructor(private formBuilder: FormBuilder){}
   ngOnInit() {
     this.contactForm = new FormGroup({
       name: new FormControl('', Validators.required), // Our existing name FormControl
-      email: new FormControl('', Validators.required), // A new FormControl for the email
-      message: new FormControl('', Validators.required)
+      email: new FormControl('', [Validators.required, Validators.email]), // A new FormControl for the email
+      message: new FormControl('', Validators.required),
+      privacyPolicy: new FormControl(false, Validators.requiredTrue)
     })
   }
 
@@ -22,5 +23,23 @@ export class ContactMeComponent {
     formData.append('name', this.contactForm.value.name as string);
     formData.append('email', this.contactForm.value.email as string);
     formData.append('message', this.contactForm.value.message as string);
+
+    this.clearInput();
+  }
+
+  clearInput(){
+    this.contactForm.reset();
+    this.sendSucces();
+  }
+
+  sendSucces(){
+    this.messageSucceed = true;
+    console.log(this.messageSucceed);
+    
+  }
+
+  isFieldValid(fieldName: string){
+    let input = this.contactForm.get(fieldName);
+    return input?.invalid && (input?.dirty || input?.touched);
   }
 }
