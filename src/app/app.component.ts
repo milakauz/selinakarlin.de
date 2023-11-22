8a5ea30ab74f5220847be07a427a7d5f7d3cc90a
Selina Karlin <karlinselina@gmail.com>
2023-11-22 11:18:17 +0100

    implementing scrollService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WindowScrollService } from './services/window-scroll.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class AppComponent {
+  private scrollSubscription!: PushSubscription;
+  constructor(private windowScrollService: WindowScrollService) { //why private not public?
+    this.scrollY$ = this.windowScrollService.scrollY$;
+  }
   title = 'selinakarlin.de';
+  scrollY$!: Observable<number>;
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    const scrollPosition = window.scrollY;
+    // console.log('position:', Math.round(scrollPosition));
+    this.windowScrollService.updateScrollY(Math.round(scrollPosition));
+  }
+
+  // getYPosition(e: Event): number {
+  //   return (e.target as Element).scrollTop;
+  // }
 }
