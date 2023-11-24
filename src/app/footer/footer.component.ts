b798088ea572e6e146088a0885a70e2b125f0099
Selina Karlin <karlinselina@gmail.com>
2023-11-24 09:15:59 +0100

    implementing footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  socialMediaList = [
+    {
+      imgSource: '../../assets/img/socials/gitHub.png', 
+      url: 'https://github.com/milakauz',
+    },
+    {
+      imgSource: '../../assets/img/socials/mail.png', 
+      url: 'karlinselina@gmail.com',
+    },
+    {
+      imgSource: '../../assets/img/socials/linkedIn.png', 
+      url: 'https://www.linkedin.com/in/selinakarlin/',
+    }
+  ]
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,43 +8,43 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
   skillsList = [
     {
-      url: '../../assets/img/icons/0_angular.png',
+      imgSource: '../../assets/img/icons/0_angular.png',
       name: 'Angular'
     },
     {
-      url: '../../assets/img/icons/1_typescript.png',
+      imgSource: '../../assets/img/icons/1_typescript.png',
       name: 'Typescript'
     },
     {
-      url: '../../assets/img/icons/2_javascript.png',
+      imgSource: '../../assets/img/icons/2_javascript.png',
       name: 'JavaScript'
     },
     {
-      url: '../../assets/img/icons/3_html.png',
+      imgSource: '../../assets/img/icons/3_html.png',
       name: 'HTML'
     },
     {
-      url: '../../assets/img/icons/4_css.png',
+      imgSource: '../../assets/img/icons/4_css.png',
       name: 'CSS'
     },
     {
-      url: '../../assets/img/icons/5_firebase.png',
+      imgSource: '../../assets/img/icons/5_firebase.png',
       name: 'Firebase'
     },
     {
-      url: '../../assets/img/icons/6_git.png',
+      imgSource: '../../assets/img/icons/6_git.png',
       name: 'Git'
     },
     {
-      url: '../../assets/img/icons/7_api.png',
+      imgSource: '../../assets/img/icons/7_api.png',
       name: 'Rest-Api'
     },
     {
-      url: '../../assets/img/icons/8_scrum.png',
+      imgSource: '../../assets/img/icons/8_scrum.png',
       name: 'Scrum'
     },
     {
-      url: '../../assets/img/icons/9_materialDesin.png',
+      imgSource: '../../assets/img/icons/9_materialDesin.png',
       name: 'Material Design'
     }
   ]
