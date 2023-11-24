23d71f522adeb2f4ea3ba595978d7eeaccb96970
Selina Karlin <karlinselina@gmail.com>
2023-11-24 12:37:33 +0100

    AOS library implementing on scroll

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,64 +1,21 @@
-import { Component, HostListener, ElementRef, ViewChild, AfterViewInit, QueryList, ViewChildren, OnInit, OnDestroy } from '@angular/core';
-import { WindowScrollService } from "./../services/window-scroll.service";
-import { Subscription } from "rxjs";
-// import { throttle } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChildren('elementToAnimate') elementsToAnimate!: QueryList<ElementRef>;
-  private scrollSubscription!: Subscription;
-  public elementsPosition: { position: number; viewable: boolean }[] = [];
-
-  constructor(private windowScrollService: WindowScrollService) {
-  }
-
+export class PortfolioComponent implements OnInit {
   ngOnInit() {
-  }
-
-  ngAfterViewInit(): void {
-    this.calculatePositions();
-
-    this.scrollSubscription = this.windowScrollService.scrollY$.subscribe(scrollPosition => {
-      this.checkPositions(scrollPosition);
-    })
-  }
-
-  checkPositions(scrollPosition: number) {
-    this.elementsPosition.forEach(element => {
-      if (scrollPosition >= element.position) {
-        console.log('Element ist im Viewport', element.position, element.viewable);
-        element.viewable = true;
-      } else if (scrollPosition <= element.position) {
-        element.viewable = false;
-        console.log('Element ist NICHT im Viewport', element.position, element.viewable);
-      }
+    Aos.init({
+      startEvent: 'scroll'
     });
   }
 
-  calculatePositions() {
-    this.elementsPosition = [];
-    // iterates through DOM-Elements to get their y-positions to be pushed in global array
-    this.elementsToAnimate.forEach((elementRef: ElementRef, index: number) => {
-      const rect = elementRef.nativeElement.getBoundingClientRect();
-      const posY = Math.round(rect.top + window.scrollY); // y position relative to document
-      // this.elementsPosition.push(posY, false)
-    })
-  }
-
-  ngOnDestroy(): void {
-    if (this.scrollSubscription) {
-      this.scrollSubscription.unsubscribe();
-    }
-  }
-
-  redirectTo(link: string){
+  redirectTo(link: string) {
     window.open(link, '_blank');
   }
-
 
   portfolioList = [
     {
