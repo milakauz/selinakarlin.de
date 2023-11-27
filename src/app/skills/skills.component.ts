34b02024f58f5a3cc46a93bb9dfd4ffebdc7e6b5
Selina Karlin <karlinselina@gmail.com>
2023-11-27 20:09:09 +0100

    reponsive design for larger screens

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,43 +8,43 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
   skillsList = [
     {
-      imgSource: '../../assets/img/icons/0_angular.png',
+      imgSource: '../../assets/img/icons/0_angular.svg',
       name: 'Angular'
     },
     {
-      imgSource: '../../assets/img/icons/1_typescript.png',
+      imgSource: '../../assets/img/icons/1_typescript.svg',
       name: 'Typescript'
     },
     {
-      imgSource: '../../assets/img/icons/2_javascript.png',
+      imgSource: '../../assets/img/icons/2_javascript.svg',
       name: 'JavaScript'
     },
     {
-      imgSource: '../../assets/img/icons/3_html.png',
+      imgSource: '../../assets/img/icons/3_html.svg',
       name: 'HTML'
     },
     {
-      imgSource: '../../assets/img/icons/4_css.png',
+      imgSource: '../../assets/img/icons/4_css.svg',
       name: 'CSS'
     },
     {
-      imgSource: '../../assets/img/icons/5_firebase.png',
+      imgSource: '../../assets/img/icons/5_firebase.svg',
       name: 'Firebase'
     },
     {
-      imgSource: '../../assets/img/icons/6_git.png',
+      imgSource: '../../assets/img/icons/6_git.svg',
       name: 'Git'
     },
     {
-      imgSource: '../../assets/img/icons/7_api.png',
+      imgSource: '../../assets/img/icons/7_api.svg',
       name: 'Rest-Api'
     },
     {
-      imgSource: '../../assets/img/icons/8_scrum.png',
+      imgSource: '../../assets/img/icons/8_scrum.svg',
       name: 'Scrum'
     },
     {
-      imgSource: '../../assets/img/icons/9_materialDesin.png',
+      imgSource: '../../assets/img/icons/9_materialDesin.svg',
       name: 'Material Design'
     }
   ]
