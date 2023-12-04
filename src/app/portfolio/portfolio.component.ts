f819ed28a803aab1c486da581ff172fdad0a273d
Selina Karlin <karlinselina@gmail.com>
2023-12-04 16:34:58 +0100

    try: fix_scrolling behaviour

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -19,20 +19,20 @@ export class PortfolioComponent implements OnInit {
 
   portfolioList = [
     {
-      url: './assets/img/portfolio/el_pollo_loco.png',
-      name: 'El Pollo Loco',
-      techStack: 'CSS | HTML | JavaScript',
-      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and bottles to fight the endboss.',
-      linkGithub: 'https://github.com/milakauz/elPolloLoco',
-      linkLive: 'https://www.elpolloloco.selinakarlin.de/index.html',
-    },
-    {
       url: './assets/img/portfolio/join.png',
       name: 'Join',
       techStack: 'CSS | HTML | JavaScript',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       linkGithub: 'https://github.com/milakauz/Join',
       linkLive: 'https://www.join.selinakarlin.de/index.html'
+    },
+    {
+      url: './assets/img/portfolio/el_pollo_loco.png',
+      name: 'El Pollo Loco',
+      techStack: 'CSS | HTML | JavaScript',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and bottles to fight the endboss.',
+      linkGithub: 'https://github.com/milakauz/elPolloLoco',
+      linkLive: 'https://www.elpolloloco.selinakarlin.de/index.html',
     }
   ]
 }
